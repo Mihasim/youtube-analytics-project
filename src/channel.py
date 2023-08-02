a2a724ba724c3c2ae1f0ed88e01b7d18ee85945c
Misha <misha.dorozhkin@mail.ru>
2023-08-02 17:29:17 +0300

    Подкорректировал для соответствия PEP8

--- src/channel.py
+++ src/channel.py
@@ -4,10 +4,10 @@ import os
 from googleapiclient.discovery import build
 
 
-
 def printj(dict_to_print: dict) -> None:
     """Выводит словарь в json-подобном удобном формате с отступами"""
     print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -18,9 +18,6 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         self.channel_id = channel_id
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-
-
-
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
